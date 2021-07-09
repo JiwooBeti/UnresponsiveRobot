3f9444713ef12ecaa481e9cca41e08458b3d156a
JiwooBeti <jiwookim10outof10@gmail.com>
2021-07-08 19:31:26 -0500

    shootauto has changed

--- src/main/java/frc/robot/commands/ShootAuto.java
+++ src/main/java/frc/robot/commands/ShootAuto.java
@@ -1,11 +1,50 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
 
-public class ShootAuto extends SequentialCommandGroup {
 
-    public ShootAuto(double pulleySpeed, double shooterLeftSpeed, double shooterRightSpeed, double transportSpeed) {
+public class ShootAuto extends CommandBase {
+
         //move pulley, shooter, transport
-        addCommands(new MovePulley(pulleySpeed), new MoveShooter(shooterLeftSpeed, shooterRightSpeed), new MoveTransport(transportSpeed));
-    }
+        private double pulleySpeed;
+        private double shooterLeftSpeed;
+        private double shooterRightSpeed;
+        private double transportSpeed;
+
+        public ShootAuto(double pulleySpeed, double shooterLeftSpeed, double shooterRightSpeed, double transportSpeed) {
+            this.pulleySpeed = pulleySpeed;
+            this.shooterLeftSpeed = shooterLeftSpeed;
+            this.shooterRightSpeed = shooterRightSpeed;
+            this.transportSpeed = transportSpeed;
+        }
+
+        public void initialize() {
+            Robot.m_robotContainer.getPulley().movePulley(pulleySpeed);
+            Robot.m_robotContainer.getShooter().moveShooter(shooterLeftSpeed, shooterRightSpeed);
+            Robot.m_robotContainer.getTransport().moveTransport(transportSpeed);
+        }
+
+        public void execute() {
+            Robot.m_robotContainer.getPulley().movePulley(pulleySpeed);
+            Robot.m_robotContainer.getShooter().moveShooter(shooterLeftSpeed, shooterRightSpeed);
+            Robot.m_robotContainer.getTransport().moveTransport(transportSpeed);
+
+        }
+
+        public boolean isFinished() {
+
+            return false;
+            //most likely wrong
+            
+
+        }
+
+        public void end() {
+            Robot.m_robotContainer.getPulley().stopPulley();
+            Robot.m_robotContainer.getShooter().stopShooter();
+            Robot.m_robotContainer.getTransport().stopTransport();
+        }
+
+        
 }
